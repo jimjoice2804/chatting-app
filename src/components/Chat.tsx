936106facb8c6cadcca83d25d6cb02fbb0399a2a
Jimmy Joice <jimjoice28@gmail.com>
2024-01-29 21:42:00 +0530

    almost done

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,14 +1,15 @@
-'use client'
+"use client";
 
-import * as Ably from 'ably';
-import { AblyProvider } from 'ably/react';
+import * as Ably from "ably";
+import { AblyProvider } from "ably/react";
+import ChatBox from "./ChatBox";
 
 export default function Chat() {
-
-  const client =  new Ably.Realtime.Promise({ authUrl: '/api' })
+  const client = new Ably.Realtime.Promise({ authUrl: "/api" });
 
   return (
-    <AblyProvider client={ client }>
+    <AblyProvider client={client}>
+      <ChatBox />
     </AblyProvider>
-  )
+  );
 }
--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,14 +1,82 @@
 import React, { useEffect, useState } from "react";
 import { useChannel } from "ably/react";
 import styles from "./ChatBox.module.css";
+import { Types as AblyTypes } from "ably";
 
 export default function ChatBox() {
+  let inputBox: HTMLElement | null = null;
+  let messageEnd: HTMLElement | null = null;
+
   const [messageText, setMessageText] = useState("");
-  const [receivedMessages, setMessages] = useState([]);
+  const [receivedMessages, setMessages] = useState<AblyTypes.Message[]>([]);
   const messageTextIsEmpty = messageText.trim().length === 0;
 
-  let inputBox = null;
-  let messageEnd = null;
+  const { channel, ably } = useChannel("chat-demo", (message) => {
+    const history = receivedMessages.slice(-199);
+    setMessages([...history, message]);
+  });
 
-  return <></>;
+  const sendChatMessage = (messageText: string) => {
+    channel.publish({ name: "chat-message", data: messageText });
+    setMessageText("");
+    inputBox?.focus();
+  };
+
+  const handleFormSubmission = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    sendChatMessage(messageText);
+  };
+
+  const handleKeyPress = (event: any) => {
+    if (event.charCode !== 13 || messageTextIsEmpty) {
+      return;
+    }
+    sendChatMessage(messageText);
+    event.preventDefault();
+  };
+
+  const messages = receivedMessages.map((message, index) => {
+    const author = message.connectionId === ably.connection.id ? "me" : "other";
+    return (
+      <span key={index} className={styles.message} data-author={author}>
+        {message.data}
+      </span>
+    );
+  });
+
+  useEffect(() => {
+    messageEnd?.scrollIntoView({ behavior: "smooth" });
+  });
+
+  return (
+    <div className={styles.chatHolder}>
+      <div className={styles.chatText}>
+        {messages}
+        <div
+          ref={(element) => {
+            messageEnd = element;
+          }}
+        ></div>
+      </div>
+      <form onSubmit={handleFormSubmission} className={styles.form}>
+        <textarea
+          ref={(element) => {
+            inputBox = element;
+          }}
+          value={messageText}
+          placeholder="Type a message..."
+          onChange={(e) => setMessageText(e.target.value)}
+          onKeyPress={handleKeyPress}
+          className={styles.textarea}
+        ></textarea>
+        <button
+          type="submit"
+          className={styles.button}
+          disabled={messageTextIsEmpty}
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
